4fc44ab94bf2a925d532015105267c7416fdd137
dgonz1206 <dgonz1206@gmail.com>
2018-10-06 02:04:10 -0700

    cleaned up code and added client

--- p2p.py
+++ p2p.py
@@ -1,83 +1,89 @@
 import select, socket, sys
 import threading
 
-class Server:
-    connections = []
-    peers = []
-    host = socket.gethostname()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# made socket class variable
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connections = []
 
-    def handler(self):
-        self.sock.listen(1)
-        c,a = self.sock.accept()
-        print(str(a[0]) + ':' + str(a[1]), "connected")
-        print(a)
-        print(c)
-        # while True:
-        #     data = c.recv(1024)
-        #     for connection in self.connections:
-        #         connection.send((data))
-        #     if not data:
-        #         print(str(a[0]) + ':' + str(a[1]), "disconnected")
-        #         self.connections.remove(c)
-        #         c.close()
-        #         break
+
+class Server:
+    peers = []
 
     def __init__(self, port):
-        #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         host = socket.gethostname()
-
-        self.sock.bind((host, port))
-
-        #sock.listen(1)
-        print("Server running...")
-        iThread = threading.Thread(target=self.handler)
+        sock.bind((host, port))
+        sock.listen(1)
+        iThread = threading.Thread(target=self.run)
         iThread.daemon = True
         iThread.start()
+        print('server is running')
         listener()
 
+    def handler(self,c,a):
+        while True:
+            data = c.recv(1024)
+            for connection in self.connections:
+                connection.send((data))
+            if not data:
+                print('IP: ', str(a[0]) + '\nPort:' + str(a[1]), "disconnected")
+                connections.remove(c)
+                c.close()
+                break
+
+    def run(self):
+        while True:
+                c, a = sock.accept()
+                cThread = threading.Thread(target=self.handler, args=(c,a))
+                cThread.daemon = True
+                cThread.start()
+                connections.append(c)
+                print('IP: ', str(a[0]) + '\nPort:' + str(a[1]), "connected")
 
 
-# class Client:
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     host = socket.gethostname()
-#
-#
-#     def sendMsg(self):
-#             while True:
-#                 self.sock.send(bytes(input(""), 'utf-8'))
-#
-#     def __init__(self, port):
-#             self.sock.connect((host, port))
-#
-#             iThread = threading.Thread(target=self.sendMsg)
-#             iThread.daemon = True
-#             iThread.start()
-#
-#             while True:
-#                     data = self.sock.recv(1024)
-#                     if not data:
-#                             break
-#                     print(data)
+
+
+
+class Client:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #host = socket.gethostname()
+
+    # def sendMsg(self):
+    #         while True:
+    #             sock.send(bytes(input(""), 'utf-8'))
+
+
+#Main problem: When we have to use the socket to connect, we can't use the inital socket we created when we started
+#the chat.py app. I get a OSError. So we have to create a new socket to connect. The problem is we lose
+#the port number associated with the old socket. Hence the new port number when the connection is establish.
+#When u run the client app, run myport then do the connect. the port number is different.
+#The prompt says:  The output should display the IP address and the listening port
+# of all the peers the process is connected to.
+    def __init__(self, IPandPort):
+        print('client started')
+        self.sock.connect(IPandPort)
+
+        #This won't work but it should cuz its using the old port
+        #sock.connect(IPandPort)
+
+        #
+        # while True:
+        #         data = self.sock.recv(1024)
+        #         if not data:
+        #                 break
+        #         print(data)
+
+
 def main(p):
     myServer = Server(int(p))
-    #sock = socket.socket()
-    #host = socket.gethostname()
-    #global port
-    #port = int(p)
-    #sock.bind((host,port))
-    #sock.listen(5)
-    #connection = None
-
+    myServer.run()
     listener()
 
 def listener():
     print("listener")
-    ex = True
     listener = input()
     listener = listener.lower()
 
-    while(ex):
+    while(True):
         if(listener == 'help'):
             help()
             break
@@ -104,38 +110,6 @@ def listener():
         else:
             break
 
-
-def listener2():
-    ex = True
-    listener = input()
-    listener = listener.lower()
-
-    while(ex):
-        if(listener == 'help'):
-            help()
-            break
-        elif(listener == 'myip'):
-            myip()
-            break
-        elif(listener == 'myport'):
-            myport()
-            break
-        elif ("connect" in listener):
-            connect(listener)
-            break
-        elif(listener == 'list'):
-            cList()
-            break
-        elif(listener == 'send'):
-            send()
-            break
-        elif(listener == 'terminate'):
-            terminate()
-            break
-        elif(listener == 'exit'):
-            exit()
-        else:
-            break
 
 def help():
     print("myip display IP address")
@@ -147,41 +121,42 @@ def help():
     print("exit exits program")
     listener()
 
+
 def myip():
     hostname = socket.gethostname()
-    global host
     host = socket.gethostbyname(hostname)
-    print("The IP address is: ", host, "\n")
+    print("The IP address is: ", sock.getsockname()[0], "\n")
     listener()
 
+
 def myport():
-    print("The program runs on port ")
+    print("The program runs on port ", sock.getsockname()[1])
     listener()
+
 
 def connect(conString):
     # parse
     socketInfo = conString.split(" ")
-    #print('IP: ',socketInfo[1:2], ' Port: ', socketInfo[-1])
-    ip = socketInfo[1]
-    #print('type:')
-    #print(type(ip))
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socketInfo[1], int(socketInfo[-1])))
-    s.sendall(bytes('IM INSIDE!', 'utf-8'))
-    #print('connected? ', s)
+    client = Client((socketInfo[1], int(socketInfo[-1])))
     listener()
 
+
 def cList():
-    print("not yet implemented")
+    print('id: \t IP address \t Port No.')
+    for x in connections:
+        print(x)
     listener()
+
 
 def send():
     print("not yet implemented")
     listener()
 
+
 def terminate():
     print("not yet implemented")
     listener()
+
 
 if __name__== "__main__":
     main(sys.argv[1])
