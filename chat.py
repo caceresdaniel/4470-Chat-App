e790fd4a0b2e0b9c7167771f4168449485a2da2a
dgonz1206 <dgonz1206@gmail.com>
2018-10-13 11:26:03 -0700

    variable name cleanup

--- chat.py
+++ chat.py
@@ -1,117 +1,120 @@
 import select, socket, sys
 import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connections = []
+listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+peers = []
 
-#server class that creates a server for each client that is launched
-#that way each client is a user
+# server class that creates a server for each client that is launched
+# that way each client is a user
 class Server:
-    #init method that starts off the server
+    # init method that starts off the server
     def __init__(self, port):
-        #getting the host name (IP) using sockets
+        # getting the host name (IP) using sockets
         host = socket.gethostname()
         try:
-            #setting the ip and port to a socket
-            sock.bind((host, port))
+            # setting the ip and port to a socket
+            listening_socket.bind((host, port))
         except WindowsError:
             print('Failed to create socket')
             exit()
-        #enable the server to accept connections by using sock.listen
-        sock.listen(1)
-        #creating a thread for the run function
-        iThread = threading.Thread(target=self.run)
-        #setting a thread to a daemon allows it to shut down while there are still threads running 
-        iThread.daemon = True
-        iThread.start()
-        listener()
+        # enable the server to accept connections by using sock.listen
+        listening_socket.listen(1)
+        # creating a thread for the run function
+        listening_thread = threading.Thread(target=self.run)
+        # setting a thread to a daemon allows it to shut down while there are still threads running
+        listening_thread.daemon = True
+        listening_thread.start()
+        menu()
 
-    #this is the part of code where the server receives the message from the client
-    def handler(self, c ,a):
-        while True:
-            #added try/except for when a client disconnects in a weird way
-            try:
-                data = c.recv(1024)
-                print(data.decode("utf-8"))
-            except socket.error:
-                print('Peer ', c.getpeername()[0] ,' terminates the connection')
-                if len(connections) > 0 and c in connections:
-                    connections.remove(c)
-                    c.close()
-                return
-            #checks to see if it receiving data from a socket if is not receiving data the socket is then removed and closed
-            if not data:
-                print('Peer ', c.getpeername()[0] ,' terminates the connection')
-                if len(connections) > 0 and c in connections:
-                    connections.remove(c)
-                    c.close()
-                break
-
-    #function that accepts the connection from the client and adds the connection to the list of sockets
+    # function that accepts the connection from the client and adds the connection to the list of sockets
     def run(self):
         while True:
-            c, a = sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(c,a))
-            cThread.daemon = True
-            cThread.start()
-            connections.append(c)
-            print('The connection to peer ', c.getpeername()[0], ' is successfully established ')
+            connection, address = listening_socket.accept()
+            print(address)
+            connection_thread = threading.Thread(target=self.connection_handler, args=(connection,))
+            connection_thread.daemon = True
+            connection_thread.start()
+            peers.append(connection)
+            print('The connection to peer ', connection.getpeername()[0], ' is successfully established ')
 
-#handles the client side of the program such as sending messages to the server, and connections
+    # this is the part of code where the server receives the message from the client
+    def connection_handler(self, connection):
+        while True:
+            # added try/except for when a client disconnects in a weird way
+            try:
+                data = connection.recv(1024)
+                print(data.decode("utf-8"))
+            except socket.error:
+                print('Peer ', connection.getpeername()[0], ' terminates the connection')
+                if len(peers) > 0 and connection in peers:
+                    peers.remove(connection)
+                    connection.close()
+                return
+            # checks to see if it receiving data from a socket if is not receiving data the socket is then removed and closed
+            if not data:
+                print('Peer ', connection.getpeername()[0], ' terminates the connection')
+                if len(peers) > 0 and connection in peers:
+                    peers.remove(connection)
+                    connection.close()
+                break
+
+
+# handles the client side of the program such as sending messages to the server, and connections
 class Client:
 
-    #this function initializes the client by creating a socket
-    def __init__(self, IPandPort):
-        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this function initializes the client by creating a socket
+    def __init__(self, ip_port):
+        connecting_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             print('Connecting...')
-            #connects to the server and sends ip and port
-            client_sock.connect(IPandPort)
+            # connects to the server and sends ip and port
+            connecting_socket.connect(ip_port)
         except WindowsError:
             print('Connection failed.')
             return
-        print('The connection to peer ', client_sock.getpeername()[0], ' is successfully established ')
-        cThread = threading.Thread(target=self.handler2, args=(client_sock,None))
-        cThread.daemon = True
-        cThread.start()
-        connections.append(client_sock)
+        print('The connection to peer ', connecting_socket.getpeername()[0], ' is successfully established ')
+        client_thread = threading.Thread(target=self.connection_handler, args=(connecting_socket,))
+        client_thread.daemon = True
+        client_thread.start()
+        peers.append(connecting_socket)
 
-    #handler used to receive the message from the server and then sends to a specific client almost same as server handler
-    def handler2(self, c, a):
+    # handler used to receive the message from the server and then sends to a specific client
+    # almost same as server handler
+    def connection_handler(self, connection):
         while True:
             try: 
-                data = c.recv(1024)
+                data = connection.recv(1024)
             except socket.error:
-                print('Peer ', c.getpeername()[0] ,' terminates the connection')
-                if len(connections) > 0 and c in connections:
-                    connections.remove(c)
-                    c.close()
+                print('Peer ', connection.getpeername()[0], ' terminates the connection')
+                if len(peers) > 0 and connection in peers:
+                    peers.remove(connection)
+                    connection.close()
                 return
             if not data:
-                print('Peer ', c.getpeername()[0], ' terminate the connection')
-                if len(connections) > 0 and c in connections:
-                    connections.remove(c)
-                    c.close()
+                print('Peer ', connection.getpeername()[0], ' terminate the connection')
+                if len(peers) > 0 and connection in peers:
+                    peers.remove(connection)
+                    connection.close()
                 break
             print(data.decode("utf-8"))
 
 
-#function that sends the message from a specific socket 
+# function that sends the message from a specific socket
 def sendMsg(index, message):
     i = index - 1
-    connections[i].send(bytes(message, 'utf-8'))
+    peers[i].send(bytes(message, 'utf-8'))
     
 
-#where the program first starts to run
-#creates instance of the server and sends the port that was passed in as an argument and
-#launches the server with that part, after server is launched, the listener is ran
+# where the program first starts to run
+# creates instance of the server and sends the port that was passed in as an argument and
+# launches the server with that part, after server is launched, the listener is ran
 def main(p):
     myServer = Server(int(p))
     myServer.run()
-    listener()
+    menu()
 
-#this function listens for user commands using simple if else statements 
-def listener():
+# this function listens for user commands using simple if else statements
+def menu():
     validCommands = ['help', 'myip', 'myport', 'list', 'terminate', 'exit', 'connect', 'send']
     listener = input()
     listener = listener.lower()
@@ -130,7 +133,7 @@ def listener():
             connect(listener)
             break
         elif listener == 'list':
-            cList()
+            connection_list()
             break
         elif "send" in listener:
             send(listener)
@@ -139,18 +142,18 @@ def listener():
             terminate(listener)
             break
         elif listener == 'exit':
-            #closes all peer connections when the exit command is given
-            for c in connections:
-                #shuts down the reading and writing side of the socket, shutdown is basically advisory to the socket at the other end that it is closing
+            # closes all peer connections when the exit command is given
+            for c in peers:
+                # shuts down the reading and writing side of the socket, shutdown is basically advisory to the socket at the other end that it is closing
                 c.shutdown(socket.SHUT_RDWR)
-                #finalizes the closing of the socket 
+                # finalizes the closing of the socket
                 c.close
             exit()
         elif listener not in validCommands:
             invalid()
             break
 
-#lists the type of commands available 
+# lists the type of commands available
 def help():
     print("myip \n   display IP address")
     print("myport \n   display Port")
@@ -159,58 +162,58 @@ def help():
     print("send \n   sends a message to a selected connected peer \n   example format - 'send 2 hello how are you doing'")
     print("terminate \n   terminates a specified connection")
     print("exit \n   exits the program")
-    listener()
+    menu()
 
-#grabs the host from the corresponding socket 
+# grabs the host from the corresponding socket
 def myip():
-    print("The IP address is: ", sock.getsockname()[0], "\n")
-    listener()
+    print("The IP address is: ", listening_socket.getsockname()[0], "\n")
+    menu()
 
-#grabs the port from the corresponging port
+# grabs the port from the corresponding port
 def myport():
-    print("The program runs on port ", sock.getsockname()[1])
-    listener()
+    print("The program runs on port ", listening_socket.getsockname()[1])
+    menu()
 
-#first parses the string received then uses it with the class client to connect 
+# first parses the string received then uses it with the class client to connect
 def connect(conString):
     socketInfo = conString.split(" ")
     client = Client((socketInfo[1], int(socketInfo[-1])))
-    listener()
+    menu()
 
-#lists all connections to and from the server using the list of sockets and a for loop
-def cList():
+# lists all connections to and from the server using the list of sockets and a for loop
+def connection_list():
     print('id: \t IP address \t Port No.')
-    for x in range(len(connections)):
-        print(x+1, '\t', connections[x].getpeername()[0], '\t',  connections[x].getpeername()[1])
-    listener()
+    for x in range(len(peers)):
+        print(x + 1, '\t', peers[x].getpeername()[0], '\t', peers[x].getpeername()[1])
+    menu()
 
-#grabs the string from the listener then parses it in to usable variables 
-#then sends it to the function that sends the message to specified client
+# grabs the string from the listener then parses it in to usable variables
+# then sends it to the function that sends the message to specified client
 def send(senString):
     sendInfo = senString.split(" ", 2)
     index = int(sendInfo[1])
     msg = ''.join(sendInfo[2:])
     sendMsg(index, msg)
-    listener()
+    menu()
 
-#terminates specific peer connection by first splitting the string received into needed values
+# terminates specific peer connection by first splitting the string received into needed values
 # it then shuts down both the reading and writing end of the specific socket, and then closes the 
-#connection, finally removing the connection from the list of sockets.
+# connection, finally removing the connection from the list of sockets.
 def terminate(termString):
     termInfo = termString.split(" ")
     c = int(termInfo[1]) - 1
-    connections[c].shutdown(socket.SHUT_RDWR)
-    connections[c].close
-    del connections[c]
-    listener()
+    peers[c].shutdown(socket.SHUT_RDWR)
+    peers[c].close
+    del peers[c]
+    menu()
 
-#just says invalid command when an invalid command was input in the program
+# just says invalid command when an invalid command was input in the program
 def invalid():
     print("Error Invalid command")
-    listener()
+    menu()
 
-#this function allows the program to start with the port initialization
-#the port is passed in when the program is first ran as an argument 
-#and is then later used to initialize the server
+# this function allows the program to start with the port initialization
+# the port is passed in when the program is first ran as an argument
+# and is then later used to initialize the server
 if __name__ == "__main__":
     main(sys.argv[1])
